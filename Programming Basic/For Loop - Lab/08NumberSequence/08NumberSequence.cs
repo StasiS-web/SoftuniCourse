d9a5344d96c0b72a2f442bc609fd1179705606d5
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-03-25 21:07:04 +0200

    Judge 100/100

---
+++ Programming Basic/For Loop - Lab/08NumberSequence/08NumberSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+            
+            for (int i = 1; i <= numberCount; i++)
+            {
+               int currentValue = int.Parse(Console.ReadLine());
+                if (currentValue >= maxNumber)
+                {
+                    maxNumber = currentValue;
+                }
+                if (currentValue <= minNumber)
+                {
+                    minNumber = currentValue;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+
+        }
+    }
+}
