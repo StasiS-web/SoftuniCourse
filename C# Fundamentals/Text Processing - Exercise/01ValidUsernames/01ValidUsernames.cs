e5694862275068f905cedea2e29cb953b9b2ea78
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-19 23:03:28 +0100

    Judge 100/100

---
+++ C# Fundamentals/Text Processing - Exercise/01ValidUsernames/01ValidUsernames.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01ValidUsernamesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                          .Split(", ");
+            List<string> validUsernames = new List<string>(); //create list with valid usernames
+
+            //go through all usernames
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                string users = usernames[i];
+                //check username's length has to be between 3 and 16 characters
+                if (users.Length >= 3 && users.Length <= 16)
+                {
+                    bool validUsername = ValidUser(users);
+
+                    //add valid uername in the list
+                    if (validUsername == true)
+                    {
+                        validUsernames.Add(users);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validUsernames));
+        }
+
+        //Boolean Method for validating of username
+        public static bool ValidUser(string users)
+        {
+            //check symbols by go through them
+            foreach (var symbol in users)
+            {
+                //contains only letters, numbers, hyphens and underscores
+                if (char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_')
+                {
+                    continue;
+                }
+                else
+                {  //if characters are different from letters, numbers, hyphens and underscores stop
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ C# Fundamentals/Text Processing - Exercise/02CharacterMultiplier/02CharacterMultiplier.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02CharacterMultiplierExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputs = Console.ReadLine()
+                  .Split(" ")
+                  .ToList();
+
+            int sum = SumCharacters(inputs[0], inputs[1]);
+            Console.WriteLine(sum);
+
+        }
+
+        //Method which get stings as arguments and return the sum of its's symbols
+        static int SumCharacters(string wordOne, string wordTwo)
+        {
+            int sum = 0;
+
+            //if string one is longer than second one
+            if (wordOne.Length > wordTwo.Length)
+            {
+                //go through all the characters in each string
+                for (int i = 0; i < wordTwo.Length; i++)
+                {
+                    //calculate the character's sum = string1[symbol] * string1[symbol]
+                    sum += wordOne[i] * wordTwo[i];
+                }
+
+                for (int i = wordTwo.Length; i < wordOne.Length; i++)
+                {
+                    //calculate the character's sum without multiplying
+                    sum += wordOne[i];
+                }
+            }
+            else if (wordOne.Length < wordTwo.Length)
+            {  //if string two is longer then the first string
+                //go through all the characters in each string
+                for (int i = 0; i < wordOne.Length; i++)
+                {
+                    //calculate the character's sum
+                    sum += wordOne[i] * wordTwo[i];
+                }
+
+                for (int i = wordOne.Length; i < wordTwo.Length; i++)
+                {
+                    //calculate the character's sum without multiplying
+                    sum += wordTwo[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < wordOne.Length; i++)
+                {
+                    //calculate the character's sum
+                    sum += wordOne[i] * wordTwo[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
