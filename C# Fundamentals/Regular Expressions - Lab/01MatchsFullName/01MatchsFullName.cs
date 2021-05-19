13e9f7832a873b808cc2febb4b082a9682cfbfb0
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-19 22:56:26 +0100

    Judge 100/100

---
+++ C# Fundamentals/Regular Expressions - Lab/01MatchsFullName/01MatchsFullName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01MatchsFullNameLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b"; //get expression
+            string fullName = Console.ReadLine();
+
+            //match it with collection
+            MatchCollection namesCollection = Regex.Matches(fullName, pattern);
+
+            foreach (var name in namesCollection)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
