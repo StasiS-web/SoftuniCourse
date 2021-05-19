41e251f720b715579fc20e24bc6959a0a8f832ce
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-19 23:07:32 +0100

    Judge 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/01ReverseStrings/01ReverseStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> charStr = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                charStr.Push(item);
+            }
+
+            while (charStr.Count > 0)
+            {
+                Console.Write(charStr.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
