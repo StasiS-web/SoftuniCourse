e8fac257e2f97d3856136d301e77fc7e0ba64d9e
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-19 21:38:49 +0100

    Judge 100/100

---
+++ C# Advanced/03MaxAndMinElement/03MaxAndMinElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03MaxAndMinElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> sequence = new Stack<int>();
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                int[] command = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                switch(command[0])
+                {
+                    case 1:
+                     //Push the element x into the stack
+                    sequence.Push(command[1]);
+                      break;
+                    case 2:
+                      // Delete the element present at the top of the stack.
+                      if (sequence.Count != 0)
+                      {
+                        sequence.Pop();
+                      }
+                        break;
+                    case 3:
+                        if (sequence.Count != 0)
+                        {
+                            //Print the maximum element in the stack.
+                            Console.WriteLine(sequence.Max());
+                        }
+                        break;
+                    case 4:
+                        if (sequence.Count != 0)
+                        {
+                            // Print the minimum element in the stack.
+                            Console.WriteLine(sequence.Min());
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", sequence));
+        }
+    }
+}
