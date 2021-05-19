ba3db53837f358dc0971e26213676c67959a3edd
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-19 22:47:10 +0100

    Judge 100/100

---
+++ C# Advanced/Stacks and Queues - Exercise/01BackStackOperations/01BackStackOperations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01BackStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            //stack's parameters
+            int push = input[0];
+            int pop = input[1];
+            int item = input[2];
+            //elements in the stack
+            int[] items = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> stackNum = new Stack<int>();
+
+            //go through all elements to push into the stack
+            for (int i = 0; i < push; i++)
+            {
+
+                stackNum.Push(items[i]);
+            }
+
+            //go through all elements to pop into the stack
+            for (int i = 0; i < pop; i++)
+            {
+
+                stackNum.Pop();
+            }
+
+            //if the items exists in the stack
+            if (stackNum.Contains(item))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                //if elemant count is zero
+                if (stackNum.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                { //atherwize print the smallest element 
+                    Console.WriteLine(stackNum.Min());
+                }
+
+            }
+        }
+    }
+}
