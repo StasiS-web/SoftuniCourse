d07d58e80b93876360ac23e9b9b9a7bc88e5aba0
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-23 11:16:49 +0100

    Judge 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/05PrintEvenNumbersSolution1/05PrintEvenNumbersSolution1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            //Queue<int> queueNum = new Queue<int>(numbers);
+            Queue<int> evenNum = new Queue<int>();
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                   
+                    evenNum.Enqueue(numbers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNum));
+        }
+    }
+}
