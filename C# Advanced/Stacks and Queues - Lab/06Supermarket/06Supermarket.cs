d6eabd2ddf8429a4068f2d245c156a5562e9ade7
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-05-23 11:59:54 +0100

    Judge 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/06Supermarket/06Supermarket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queueNames = new Queue<string>();
+
+            string command = Console.ReadLine();
+
+            while(command != "End")
+            { 
+                if (command == "Paid")
+                {  //print every customer name and empty the queue
+                    Console.WriteLine(string.Join(Environment.NewLine, queueNames));
+                    queueNames.Clear();
+                } 
+                else
+                { //adds the names in the queue
+                    queueNames.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queueNames.Count} people remaining.");
+        }
+    }
+}
