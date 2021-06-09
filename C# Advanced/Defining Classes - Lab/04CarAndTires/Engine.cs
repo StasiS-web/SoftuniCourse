2bee953df630ea7c243c0a16c43a8a4dcec5adfb
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-06-09 23:39:06 +0100

    Judge 100/100

---
+++ C# Advanced/Defining Classes - Lab/04CarAndTires/Engine.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    public class Engine
+    {
+        //Fields
+        private int horsePower;
+        private double cubicCapacity;
+
+        //Constructors
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        //Properties
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+    }
+}
---
+++ C# Advanced/Defining Classes - Lab/04CarAndTires/Tire.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    public class Tire
+    {
+        //Fields
+        private int year;
+        private double pressure;
+
+        //Constructors
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        //Properties
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+    }
+}
