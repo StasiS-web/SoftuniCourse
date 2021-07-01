772d62f1d47b412caae5d2844f1acb27bba840f7
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-07-01 17:32:40 +0100

    Judge 100/100

---
+++ C# OOP/Encapsulation - Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] commandArg = Console.ReadLine().Split();
+                Person person = new Person(commandArg[0], commandArg[1], int.Parse(commandArg[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
---
+++ C# OOP/Encapsulation - Lab/SalaryIncrease/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal bonus)
+        {
+            if (Age > 30)
+            {
+                //get half the increase
+                Salary += Salary * bonus / 100;
+            }
+            else
+            {
+                Salary += Salary * bonus / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
