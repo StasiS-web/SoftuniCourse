6c6b4cda05cc2ec225a169ec3a00c9a26c201a9c
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-07-09 18:01:45 +0300

    Judge 100/100

---
+++ C# OOP/Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public double Heigth
+        {
+            get => height;
+            private set
+            {
+                height = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                width = value;
+            }
+        }
+
+        public override double CalculateArea() => Heigth * Width;
+
+        public override double CalculatePerimeter() => 2 * (Heigth + Width);
+
+        public override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}
