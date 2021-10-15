d3789726db6e2851356b88919e52af1772e1bf71
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-10-15 14:47:47 +0100

    Update Minions.sql

--- MS SQL/01.Databases Introduction/Minions.sql
+++ MS SQL/01.Databases Introduction/Minions.sql
@@ -1,10 +1,10 @@
--- Create Database
+---Task 01: Create Database
 CREATE DATABASE [Minions]
 
 USE [Minions]
 GO
 
--- Create tables
+---Task 02: Create tables
 CREATE TABLE [Minions](
     [ID] INT NOT NULL PRIMARY KEY,
     [Name] NVARCHAR(50) NOT NULL,
@@ -16,12 +16,14 @@ CREATE TABLE [Towns](
     [Name] NVARCHAR(50) NOT NULL
  )
 
+---Task 03: Alter Minions Table
 ALTER TABLE [Minions]
 		ADD [TownID] INT
 
    ALTER TABLE [Minions]
 ADD CONSTRAINT [FK_MinionsID] FOREIGN KEY (TownId) REFERENCES [Towns]([Id])
 
+---Task 04: Insert Records in Both Tables
 INSERT INTO [Towns]([ID], [Name]) 
 VALUES (1, 'Sofia'),
        (2, 'Plovdiv'),
@@ -32,6 +34,7 @@ VALUES (1, 'Kevin', 22, 1),
        (2, 'Bob', 15, 3),
        (3, 'Steward', NULL, 2)
 
+---Task 05: Truncate Table Minions
 TRUNCATE TABLE 'Minions'
 	DROP TABLE 'Minions'
 
