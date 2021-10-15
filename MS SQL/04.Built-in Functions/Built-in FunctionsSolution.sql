64c8e16cffd9401a6dd233cec89b4a8804e6e20b
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-10-15 15:02:41 +0100

    Update Built-in FunctionsSolution.sql

--- MS SQL/04.Built-in Functions/Built-in FunctionsSolution.sql
+++ MS SQL/04.Built-in Functions/Built-in FunctionsSolution.sql
@@ -1,62 +1,62 @@
 USE [SoftUni]
 GO
 
---- Task 01
+---Task 01: Find Names of All Employees by First Name
 SELECT [FirstName], [LastName] FROM [Employees]
 WHERE [FirstName] LIKE('Sa%')
 
 SELECT [FirstName], [LastName] FROM [Employees]
 WHERE LEFT ([FirstName], 2) = 'Sa'
 
---- Task 02
+---Task 02: Find Names of All Employees by Last Name 
 SELECT [FirstName], [LastName] 
   FROM [Employees]
  WHERE [LastName] LIKE ('%ei%')
 
----Task 03
+---Task 03: Find First Names of All Employess
 SELECT [FirstName] 
   FROM [Employees]
 WHERE [DepartmentID] = 3 OR [DepartmentID] = 10 AND YEAR([HireDate]) BETWEEN 1995 AND 2005
 
----Task 04
+---Task 04: Find All Employees Except Engineers
 SELECT [FirstName], [LastName] 
   FROM [Employees]
  WHERE [JobTitle] NOT LIKE ('%engineer%')
  
- ---Task 05
+---Task 05: Find Towns with Name Length
  SELECT [Name] FROM [Towns]
  WHERE LEN([Name]) = 5 OR LEN([Name]) = 6
  ORDER BY [Name] ASC 
  
- ---Task 06
+ ---Task 06: Find Towns Starting With
  SELECT [TownID], [Name] FROM [Towns]
  WHERE [Name] LIKE ('[mkbe]%')
  ORDER BY [Name]
  
- ---Task 07
+ ---Task 07: Find Towns Not Starting With
  SELECT [TownID], [Name] FROM [Towns]
  WHERE [Name] NOT LIKE ('[rbd]%')
  ORDER BY [Name]
  
- ---Task 08
+---Task 08: Create View Employees Hired After
  CREATE  VIEW [V_EmployeesHiredAfter2000] AS
     SELECT [FirstName], [LastName] 
       FROM [Employees]
      WHERE YEAR([HireDate]) > 2000 
      
----Task 09
+---Task 09: Length of Last Name
 SELECT [FirstName], [LastName] 
   FROM [Employees]
 WHERE LEN([LastName]) = 5
 
----Task 10
+---Task 10: Rank Employees by Salary
 SELECT [EmployeeID] , [FirstName], [LastName], [Salary],
 DENSE_RANK() OVER(partition by [Salary] ORDER BY [EmployeeID]) AS [Rank]
   FROM [Employees]
  WHERE [Salary] BETWEEN 10000 AND 50000
  ORDER BY [Salary] DESC 
  
----Task 12
+---Task 12: Countries Holding 'A' 
 USE [Geography]
 GO
 
@@ -65,7 +65,7 @@ SELECT [CountryName], [ISOCode]
 WHERE  [CountryName] LIKE ('%A%A%A%')
 ORDER BY [ISOCode] 
  
----Task 14
+---Task 14: Games From 2011 and 2012 Year 
 USE [Diablo]
 GO
  
@@ -74,7 +74,7 @@ SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS 'Start'
  WHERE YEAR([Start]) BETWEEN 2011 AND 2012
  ORDER BY [Start]
  
----Task 18
+---Task 18: Orders Table
 USE [Orders]
 GO
 
