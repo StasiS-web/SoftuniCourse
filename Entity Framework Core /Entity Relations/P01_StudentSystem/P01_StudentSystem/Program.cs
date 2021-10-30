66962b7fc05337b22475e77afee6a3aa8a85c465
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-10-31 00:56:21 +0100

    75/100

---
+++ Entity Framework Core /Entity Relations/P01_StudentSystem/P01_StudentSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StudentSystemContext context = new StudentSystemContext();
+
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Databse is created successfully!");
+            Console.WriteLine("Do you want to delete database(Y/N)?");
+            string result = Console.ReadLine();
+
+            if (result == "Y")
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
