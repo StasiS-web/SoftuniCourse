ee4f9e781133e80cae1e5dd35f4c8dbb9a5893e5
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-10-31 00:53:31 +0100

    Judge 100/100

---
+++ Entity Framework Core /Entity Relations/P03_FootballBetting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FootballBettingContext dbContext = new FootballBettingContext();
+
+            dbContext.Database.EnsureCreated();
+
+            Console.WriteLine("Database was created successfully");
+            Console.WriteLine("Do you want to delete the database (Y/N)?");
+
+            string result = Console.ReadLine();
+
+            if (result == "Y")
+            {
+                dbContext.Database.EnsureDeleted();
+            }
+        }
+    }
+}
