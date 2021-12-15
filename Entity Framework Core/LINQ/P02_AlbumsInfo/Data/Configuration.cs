70c4a3465b3ae2743baa10e9e846580d34d9fb14
Stanislava Stoeva <64635613+StasiS-web@users.noreply.github.com>
2021-12-15 15:45:31 +0200

    Delete Entity Framework Core/LINQ directory

--- Entity Framework Core/LINQ/P02_AlbumsInfo/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=localhost,1433;Database=MusicHub;Trusted_Connection=True";
-    }
-}
