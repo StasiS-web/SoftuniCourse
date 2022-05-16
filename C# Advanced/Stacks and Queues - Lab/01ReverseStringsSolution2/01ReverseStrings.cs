45126ea73a8c2d788e5e088865678632f4f00842
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-05-17 00:50:10 +0300

    Judge 100/100

---
+++ C# Advanced/Stacks and Queues - Lab/01ReverseStringsSolution2/01ReverseStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace _01ReverseStringsSolution2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            input = Reverse(input);
+            Console.Write(input);
+        }
+
+        public static String Reverse(String input)
+        {
+            char[] chars = input.ToCharArray();
+            int size = chars.Length;
+            Stack stack = new Stack(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                stack.Push(chars[i]);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                chars[i] = (char)stack.Pop();
+            }
+
+            return String.Join("", chars);
+        }
+    }
+}
