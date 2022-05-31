4d2163240950e1756b7e251ac6f45074965e050c
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-06-01 00:57:41 +0100

    Judge 100/100

--- C# Advanced/Stacks and Queues - Lab/02StackSum/02StackSum.cs
+++ C# Advanced/Stacks and Queues - Lab/02StackSum/02StackSum.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace _02StackSum
 {
-    class Program
+    class StackSum
     {
         static void Main(string[] args)
         {
