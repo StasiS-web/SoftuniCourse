f6e5da123bb2adbb4b0da80bb6bbe51d652331e0
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-06-01 00:59:08 +0100

    Judge 100/100

--- C# Advanced/Stacks and Queues - Lab/05PrintEvenNumbersSolution2/05PrintEvenNumbersSolution2.cs
+++ C# Advanced/Stacks and Queues - Lab/05PrintEvenNumbersSolution2/05PrintEvenNumbersSolution2.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace _05PrintEvenNumbers
 {
-    class Program
+    class PrintEvenNumbersSolution2
     {
         static void Main(string[] args)
         {
