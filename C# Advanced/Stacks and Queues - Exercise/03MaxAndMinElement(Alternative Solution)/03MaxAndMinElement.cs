a36c42ada64da5f5604a82630e97543c1aef12f1
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-06-01 00:51:46 +0100

    Judge 100/100

--- C# Advanced/Stacks and Queues - Exercise/03MaxAndMinElement(Alternative Solution)/03MaxAndMinElement.cs
+++ C# Advanced/Stacks and Queues - Exercise/03MaxAndMinElement(Alternative Solution)/03MaxAndMinElement.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace _03MaxAndMinElement
 {
-    public class _03MaxAndMinElement
+    public class MaxAndMinElement
     {
         static void Main(string[] args)
         {
